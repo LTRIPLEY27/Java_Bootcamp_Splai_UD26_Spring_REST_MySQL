f049c4cf7e2f5d442c148f60e611fd560279b4ba
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-02-28 19:17:07 +0100

    4

--- Investigadores/src/main/java/com/example/demo/ServletInitializer.java
+++ Investigadores/src/main/java/com/example/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LosInvestigadoresApplication.class);
+		return application.sources(InvestigadoresApplication.class);
 	}
 
 }
--- Investigadores/src/test/java/com/example/demo/InvestigadoresApplicationTests.java
+++ Investigadores/src/test/java/com/example/demo/InvestigadoresApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LosInvestigadoresApplicationTests {
+class InvestigadoresApplicationTests {
 
 	@Test
 	void contextLoads() {
